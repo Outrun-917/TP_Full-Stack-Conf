19c60e608d0c97dca614aa2f21dd3a85ec0a1106
Outrun <outrun.917@gmail.com>
2025-06-26 09:27:56 +0200

    aaaaaiiiie

--- 01_startup-files/js/app.js
+++ 01_startup-files/js/app.js
@@ -1,40 +1,76 @@
-const $inputName = document.querySelector("#name")
-const $jobTitleSelect = document.querySelector("#title")
-const $inputOtherTitle = document.querySelector("#other-title")
-const $designSelect = document.querySelector("#design")
-const $puns = document.querySelector(".puns")
-const $heart = document.querySelector(".heart")
-const $base = document.querySelector(".base")
+const $inputName = document.querySelector("#name");
+const $jobTitleSelect = document.querySelector("#title");
+const $inputOtherTitle = document.querySelector("#other-title");
+const $designSelect = document.querySelector("#design");
+const $totalPrice = document.querySelector(".totalPrice")
 
-console.log($inputName)
-console.log($jobTitleSelect)
+const $puns = document.querySelectorAll(".puns");
+const $heart = document.querySelectorAll(".heart");
+const $base = document.querySelector(".base");
+const $activitiesPrice = document.querySelectorAll(".activities input");
+
+let totalPrice = 0
+
+// console.log($inputName);
+// console.log($jobTitleSelect);
+
+// console.log($puns);
+// console.log($heart);
+
+console.log($activitiesPrice);
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("Hello");
-  $inputName.focus()
+  $inputName.focus();
 
-  $jobTitleSelect.addEventListener("change", function(e) {
-    console.log(e.target.value)
+  $jobTitleSelect.addEventListener("change", function (e) {
+    console.log(e.target.value);
 
     if (e.target.value === "other") {
-        $inputOtherTitle.classList.remove("is-hidden")
+      $inputOtherTitle.classList.remove("is-hidden");
     } else {
-        $inputOtherTitle.classList.add("is-hidden")
+      $inputOtherTitle.classList.add("is-hidden");
     }
-  })
+  });
 
-  $designSelect.addEventListener("change", function(e) {
-    console.log(e.target.value)
-
+  $designSelect.addEventListener("change", function (e) {
+    console.log(e.target.value);
     if (e.target.value === "js puns") {
-        $puns.classList.remove("is-hidden")
-        $heart.classList.add("is-hidden")
+      for (let i = 0; i < $puns.length; i++) {
+        $puns[i].classList.remove("is-hidden");
+      }
+
+      for (let i = 0; i < $heart.length; i++) {
+        $heart[i].classList.add("is-hidden");
+      }
     } else if (e.target.value === "heart js") {
-        $puns.classList.add("is-hidden")
-        $heart.classList.remove("is-hidden")
+      for (let i = 0; i < $puns.length; i++) {
+        $puns[i].classList.add("is-hidden");
+      }
+
+      for (let i = 0; i < $heart.length; i++) {
+        $heart[i].classList.remove("is-hidden");
+      }
     } else {
-        $puns.classList.remove("is-hidden")
-        $heart.classList.remove("is-hidden")
+      for (let i = 0; i < $puns.length; i++) {
+        $puns[i].classList.remove("is-hidden");
+      }
+
+      for (let i = 0; i < $heart.length; i++) {
+        $heart[i].classList.remove("is-hidden");
+      }
     }
-  })
+  });
+
+  for (let i = 0; i < $activitiesPrice.length; i++) {
+    $activitiesPrice[i].addEventListener("change", function (e) {
+      const price = parseInt(e.target.getAttribute("data-price"))
+      if (e.target.checked) {
+        totalPrice += price
+      } else {
+        totalPrice -= price
+      }
+      console.log(totalPrice)
+      $totalPrice.textContent = totalPrice
+    });
+  }
 });
